5aa1fc70cd3494ad596eee9dbf24231e17d95951
Lama Naser <Lamazahi40@gmail.com>
2024-04-29 17:39:48 +0300

    Delete section 2 directory

--- section 2/main.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-image = cv2.imread("image.jpg",cv2.IMREAD_GRAYSCALE)
-mean = 0
-variance = 40
-segma = np.sqrt(variance)
-noisy_image = image + np.random.normal(mean,segma,image.shape)
-cv2.imwrite("noisy_image.jpg",noisy_image)
