9bb8d0089d427da31ad52b2c6e1f600660155df4
Lama Naser <Lamazahi40@gmail.com>
2024-04-29 17:39:34 +0300

    Delete section 1 directory

--- section 1/main.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-image = cv2.imread("image.jpg",cv2.IMREAD_GRAYSCALE)
-gama = 0.4
-newImage = np.power(image/255.0,gama) * 255.0
-#convert pixel values to 8-bit unsigned integers
-newImage = np.uint8(newImage)
-cv2.imwrite("powered_Image.jpg",newImage)
